8c01107a6525b9029e31476c2f5076d9f0b48ea0
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-22 08:06:18 +0100

    Working on the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main1";
 
-export default function App () {
+export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
-  )
+  );
 }

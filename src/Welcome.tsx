9e4add85a7f15d5c33d466e66c1e12e66c647050
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-23 06:01:06 +0000

    Another day starts, working on this project ;

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -6,7 +6,7 @@ interface WelcomeProps {
 
 export default function Welcome({children}: WelcomeProps) {
   return (
-    <div>
+    <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h4>15 Questions to test your React Mastery</h4>
       {children}
